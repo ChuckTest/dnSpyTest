6ee246f4adfc9ef288a96e84937ce8d61b00e65a
Chuck Lu <chuck.lu@qq.com>
2019-08-11 15:29:00 +0800

    add method for async test

---
+++ dnSpyTest/AsyncTest.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+namespace dnSpyTest
+{
+    public class AsyncTest
+    {
+        public static async Task<T> RequestResultAsync<T>()
+        {
+            return await Task.Run(async delegate {
+                return default(T);
+            });
+        }
+    }
+}
