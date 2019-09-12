f00eb32b6d014badb952277d3d6f9f085a235012
Chuck Lu <chuck.lu@qq.com>
2019-09-12 09:51:37 +0800

    convert project to console app

---
+++ dnSpyTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace dnSpyTest
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello world");
+            Console.ReadLine();
+        }
+    }
+}
